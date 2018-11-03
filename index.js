a7d895c8ead97e03b5f49238316aa013224f3261
Scott Ian <sim2487@gmail.com>
2018-11-03 16:46:58 -0700

    added store object

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 /*eslint-env jquery*/
 'use strict';
 //////////////////////
+const STORE = {
+  //structuring STORE as an object now as we will need to do it
+  //later anyway.
+  ITEMS: [//add unique ID for each item in ITEMS?
+    {name: 'apples', checked: false},
+    {name: 'oranges', checked: false},
+    {name: 'milk', checked: true},
+    {name: 'bread', checked: false},
+  ],
+
+};
+//////////////
 function renderShoppingList() {
   console.log('renderShoppingList() fired');
 }
